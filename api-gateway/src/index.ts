973d32ae85cd77bd82814966888d61bc933c9005
AbdallahRdf <abdallahradfi@gmail.com>
2025-05-24 12:37:33 +0100

    Integrate Datadog monitoring with Kubernetes via Helm and custom DatadogAgent manifest

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -1,7 +1,9 @@
-import express from 'express';
+import express, { Request } from 'express';
 import usersRoutes from './routes/users.routes';
 import tasksRoutes from './routes/tasks.routes';
 import cookieParser from 'cookie-parser';
+import fs from 'fs';
+import path from 'path';
 import morgan from 'morgan';
 import cors from 'cors';
 import { errorHandler } from './middlewares/errorHandler';
@@ -13,7 +15,26 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+// app.use(morgan('dev', {stream: accessLogStream}));
+//* Use morgan in JSON format
+morgan.token('body', (req: Request) => JSON.stringify(req.body));
+
+app.use(
+  morgan((tokens, req, res) => {
+    return JSON.stringify({
+      method: tokens.method(req, res),
+      url: tokens.url(req, res),
+      status: Number(tokens.status(req, res)),
+      content_length: tokens.res(req, res, 'content-length'),
+      response_time: `${tokens['response-time'](req, res)} ms`,
+      date: tokens.date(req, res, 'iso'),
+    });
+  }, { stream: accessLogStream })
+);
+
 
 // Mount the services
 app.use("/api/tasks", tasksRoutes);
